c2bab76c6f9ff9151455677ada249e23356f8592
annakhuseinova <annakhuseinova@yandex.ru>
2018-10-03 00:43:18 +0300

    ДЗ к уроку 5. Звуковые эффекты и музыка

--- core/src/com/mygdx/game/screen/MenuScreenRealGame.java
+++ core/src/com/mygdx/game/screen/MenuScreenRealGame.java
@@ -115,7 +115,6 @@ public class MenuScreenRealGame extends BasicStarShooterScreen implements Action
         if (source == exitButton){
             Gdx.app.exit();
         }else if (source == playButton){
-
             mainScreenMusic.stop();
             game.setScreen(new GameScreen(game));
             gameMusic = Gdx.audio.newMusic(Gdx.files.internal("data/gamemusic.mp3"));
